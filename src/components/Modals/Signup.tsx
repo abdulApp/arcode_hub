51812350233706bc591ac26c5668bbbf52f567c8
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2023-06-30 00:07:48 +0300

    signup functionality

--- src/components/Modals/Signup.tsx
+++ src/components/Modals/Signup.tsx
@@ -1,23 +1,57 @@
 import { authModalState } from "@/atoms/authModalAtom";
-import React from "react";
+import { auth, firestore } from "@/firebase/firebase";
+import { useEffect, useState } from "react";
 import { useSetRecoilState } from "recoil";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { useRouter } from "next/router";
+import { doc, setDoc } from "firebase/firestore";
+// import { toast } from "react-toastify";
 
 type SignupProps = {};
 
 const Signup: React.FC<SignupProps> = () => {
-
   const setAuthModalState = useSetRecoilState(authModalState);
-
   const handleClick = () => {
     setAuthModalState((prev) => ({ ...prev, type: "login" }));
   };
+  const [inputs, setInputs] = useState({
+    email: "",
+    displayName: "",
+    password: "",
+  });
+  const router = useRouter();
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
+  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleRegister = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!inputs.email || !inputs.password || !inputs.displayName)
+      return alert("Please fill all fields");
+    try {
+      console.log("here");
+      const newUser = await createUserWithEmailAndPassword(
+        inputs.email,
+        inputs.password
+      );
+      console.log("here1");
+      if (!newUser) return;
+      router.push("/");
+    } catch (error: any) {
+      alert(error.message);
+    }
+    console.log("regist");
+  };
+
+  useEffect(() => {
+    if (error) alert(error.message);
+  }, [error]);
 
   return (
-    <form className="space-y-6 px-6 pb-4">
-      <h3 className="text-xl font-medium text-white uppercase">
-        Registered to leetclone
-      </h3>
-      {/* EMAIL */}
+    <form className="space-y-6 px-6 pb-4" onSubmit={handleRegister}>
+      <h3 className="text-xl font-medium text-white">Register to LeetClone</h3>
       <div>
         <label
           htmlFor="email"
@@ -26,30 +60,36 @@ const Signup: React.FC<SignupProps> = () => {
           Email
         </label>
         <input
+          onChange={handleChangeInput}
           type="email"
           name="email"
           id="email"
-          className="border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white"
+          className="
+        border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5
+        bg-gray-600 border-gray-500 placeholder-gray-400 text-white
+    "
           placeholder="name@company.com"
         />
       </div>
-      {/* NAME */}
       <div>
         <label
-          htmlFor="displayname"
+          htmlFor="displayName"
           className="text-sm font-medium block mb-2 text-gray-300"
         >
           Display Name
         </label>
         <input
-          type="displayname"
-          name="displayname"
-          id="displayname"
-          className="border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white"
-          placeholder="Abdul Alamoudi"
+          onChange={handleChangeInput}
+          type="displayName"
+          name="displayName"
+          id="displayName"
+          className="
+        border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5
+        bg-gray-600 border-gray-500 placeholder-gray-400 text-white
+    "
+          placeholder="John Doe"
         />
       </div>
-      {/* PASSWORD */}
       <div>
         <label
           htmlFor="password"
@@ -58,24 +98,34 @@ const Signup: React.FC<SignupProps> = () => {
           Password
         </label>
         <input
+          onChange={handleChangeInput}
           type="password"
           name="password"
           id="password"
-          className="border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white"
-          placeholder="********"
+          className="
+        border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5
+        bg-gray-600 border-gray-500 placeholder-gray-400 text-white
+    "
+          placeholder="*******"
         />
       </div>
 
       <button
         type="submit"
-        className="w-full text-white focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-brand-orange hover:bg-brand-orange-s"
+        className="w-full text-white focus:ring-blue-300 font-medium rounded-lg
+            text-sm px-5 py-2.5 text-center bg-brand-orange hover:bg-brand-orange-s
+        "
       >
-         Registered
+        {loading ? "Registering..." : "Register"}
       </button>
-      
+
       <div className="text-sm font-medium text-gray-300">
         Already have an account?{" "}
-        <a href="#" className="text-blue-700 hover:underline" onClick={handleClick}>
+        <a
+          href="#"
+          className="text-blue-700 hover:underline"
+          onClick={handleClick}
+        >
           Log In
         </a>
       </div>
--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyA85SsrTfIH8P4lOl3P-NBJ6pyBE5Wgn8Y",
+  authDomain: "leetcode-d9ffb.firebaseapp.com",
+  projectId: "leetcode-d9ffb",
+  storageBucket: "leetcode-d9ffb.appspot.com",
+  messagingSenderId: "751081271643",
+  appId: "1:751081271643:web:9d3bd156ef6f910fb66a7e",
 };
 
 const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
