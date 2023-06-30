89ea710d4de7eee6d3d045bebfbf9c42b92deba8
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2023-06-30 10:27:19 +0300

    login functionality

--- src/components/Modals/Login.tsx
+++ src/components/Modals/Login.tsx
@@ -1,5 +1,8 @@
 import { authModalState } from "@/atoms/authModalAtom";
-import React from "react";
+import { auth } from "@/firebase/firebase";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { useSetRecoilState } from "recoil";
 
 type LoginProps = {};
@@ -7,14 +10,52 @@ type LoginProps = {};
 const Login: React.FC<LoginProps> = () => {
   const setAuthModalState = useSetRecoilState(authModalState);
 
-  const handleClick = (type:"login" | "register" | "forgotPassword") => {
+  const handleClick = (type: "login" | "register" | "forgotPassword") => {
     setAuthModalState((prev) => ({ ...prev, type }));
   };
+
+  const [inputs, setInputs] = useState({ email: "", password: "" });
+
+  const router = useRouter();
+
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!inputs.email || !inputs.password)
+      return alert("Please fill all fields");
+    try {
+      const newUser = await signInWithEmailAndPassword(
+        inputs.email,
+        inputs.password
+      );
+      if (!newUser) return;
+      router.push("/");
+    } catch (error: any) {
+      alert(error.message);
+      // error(error.message, {
+      //   position: "top-center",
+      //   autoClose: 3000,
+      //   theme: "dark",
+      // });
+    }
+  };
+
+  console.log(user);
+
+  useEffect(() => {
+    if (error) alert(error.message);
+    // toast.error(error.message, { position: "top-center", autoClose: 3000, theme: "dark" });
+  }, [error]);
+
   return (
-    <form className="space-y-6 px-6 pb-4">
-      <h3 className="text-xl font-medium text-white uppercase">
-        sign in to leetclone
-      </h3>
+    <form className="space-y-6 px-6 pb-4" onSubmit={handleLogin}>
+      <h3 className="text-xl font-medium text-white">Sign in to LeetClone</h3>
       <div>
         <label
           htmlFor="email"
@@ -23,10 +64,14 @@ const Login: React.FC<LoginProps> = () => {
           Your Email
         </label>
         <input
+          onChange={handleInputChange}
           type="email"
           name="email"
           id="email"
-          className="border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white"
+          className="
+            border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5
+            bg-gray-600 border-gray-500 placeholder-gray-400 text-white
+        "
           placeholder="name@company.com"
         />
       </div>
@@ -38,21 +83,30 @@ const Login: React.FC<LoginProps> = () => {
           Your Password
         </label>
         <input
+          onChange={handleInputChange}
           type="password"
           name="password"
           id="password"
-          className="border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white"
-          placeholder="********"
+          className="
+            border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5
+            bg-gray-600 border-gray-500 placeholder-gray-400 text-white
+        "
+          placeholder="*******"
         />
       </div>
 
       <button
         type="submit"
-        className="w-full text-white focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-brand-orange hover:bg-brand-orange-s"
+        className="w-full text-white focus:ring-blue-300 font-medium rounded-lg
+                text-sm px-5 py-2.5 text-center bg-brand-orange hover:bg-brand-orange-s
+            "
       >
-        Login
+        {loading ? "Loading..." : "Log In"}
       </button>
-      <button className="flex w-full justify-end" onClick={() => handleClick("forgotPassword")}>
+      <button
+        className="flex w-full justify-end"
+        onClick={() => handleClick("forgotPassword")}
+      >
         <a
           href="#"
           className="text-sm block text-brand-orange hover:underline w-full text-right"
@@ -62,7 +116,11 @@ const Login: React.FC<LoginProps> = () => {
       </button>
       <div className="text-sm font-medium text-gray-300">
         Not Registered?{" "}
-        <a href="#" className="text-blue-700 hover:underline" onClick={() => handleClick("register")}>
+        <a
+          href="#"
+          className="text-blue-700 hover:underline"
+          onClick={() => handleClick("register")}
+        >
           Create account
         </a>
       </div>
