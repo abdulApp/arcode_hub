3169c59693fb7d4c9c28390fb09ddf24477adff5
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2023-06-30 15:25:35 +0300

    problems table UI 'finished'

--- src/components/ProblemsTable.tsx
+++ src/components/ProblemsTable.tsx
@@ -2,6 +2,7 @@ import { problems } from "@/mockProblems/problems";
 import Link from "next/link";
 import React from "react";
 import { BsCheckCircle } from "react-icons/bs";
+import { AiFillYoutube } from "react-icons/ai";
 
 type ProblemsTableProps = {};
 
@@ -9,6 +10,12 @@ const ProblemsTable: React.FC<ProblemsTableProps> = () => {
   return (
     <tbody className="text-white">
       {problems.map((doc, idx) => {
+        const difficulyColor =
+          doc.difficulty === "Easy"
+            ? "text-dark-green-s"
+            : doc.difficulty === "Medium"
+            ? "text-dark-yellow"
+            : "text-dark-pink";
         return (
           <tr
             className={`${idx % 2 == 1 ? "bg-dark-layer-1" : ""}`}
@@ -18,9 +25,27 @@ const ProblemsTable: React.FC<ProblemsTableProps> = () => {
               <BsCheckCircle fontSize={"18"} width="18" />
             </th>
             <td className="px-6 py-4">
-              <Link className="hover:text-blue-600 cursor-pointer" href={`/problems/${doc.id}`}>
+              <Link
+                className="hover:text-blue-600 cursor-pointer"
+                href={`/problems/${doc.id}`}
+              >
                 {doc.title}
               </Link>
+            </td>
+            <td className={`px-6 py-4 ${difficulyColor}`}>{doc.difficulty}</td>
+            <td className="px-6 py-4">{doc.category}</td>
+            <td className="px-6 py-4">
+              {doc.videoId ? (
+                <AiFillYoutube
+                  fontSize={"28"}
+                  className="cursor-pointer hover:text-red-600"
+                  // onClick={() =>
+                  // 	setYoutubePlayer({ isOpen: true, videoId: problem.videoId as string })
+                  // }
+                />
+              ) : (
+                <p className="text-gray-400">Coming soon</p>
+              )}
             </td>
           </tr>
         );
