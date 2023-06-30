9b962e9b3ff7c0d39d8faef0b9ed43f016aa051f
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2023-06-30 14:32:39 +0300

    Home Page UI 'topbar and bg'

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Topbar from "@/components/Topbar/Topbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-      <main>
-        hi
+      <main className="bg-dark-layer-2 min-h-screen">
+        <Topbar />
       </main>
     </>
   );
