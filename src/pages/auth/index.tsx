999b86f4925ebc3d3dedda90f306a133f9040979
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2023-06-30 10:39:19 +0300

    Auth Page Route Guard

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,13 +1,27 @@
 import { authModalState } from "@/atoms/authModalAtom";
 import AuthModal from "@/components/Modals/AuthModal";
 import Navbar from "@/components/Navbar/Navbar";
-import React from "react";
+import { auth } from "@/firebase/firebase";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { useRecoilValue } from "recoil";
 
 type AuthPageProps = {};
 
 const AuthPage: React.FC<AuthPageProps> = () => {
   const authModal = useRecoilValue(authModalState);
+  const [user, loading, error] = useAuthState(auth);
+  const [pageLoading, setPageLoading] = useState(true);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user) router.push("/");
+    if (!loading && !user) setPageLoading(false);
+  }, [user, router, loading]);
+
+  if (pageLoading) return null;
 
   return (
     <div className="h-screen relative bg-gradient-to-b from-gray-600 to-black">
