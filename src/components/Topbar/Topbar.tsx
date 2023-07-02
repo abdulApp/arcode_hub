c3d690c5d9f528c7cdaebe907db7d9fe32c9b0a4
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2023-07-02 20:31:25 +0300

    topbar update on auth

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -1,9 +1,12 @@
+import { auth } from "@/firebase/firebase";
 import Link from "next/link";
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 type TopbarProps = {};
 
 const Topbar: React.FC<TopbarProps> = () => {
+  const [user] = useAuthState(auth);
   return (
     <nav className="relative flex h-[50px] w-full shrink-0 items-center px-5 bg-dark-layer-1 text-dark-gray-7">
       <div
@@ -13,8 +16,6 @@ const Topbar: React.FC<TopbarProps> = () => {
         <Link href="/" className="h-[22px] flex-1">
           <img src="/logo-full.png" alt="Logo" height={100} width={100} />
         </Link>
-
-        
 
         <div className="flex items-center space-x-4 flex-1 justify-end">
           <div>
@@ -27,7 +28,7 @@ const Topbar: React.FC<TopbarProps> = () => {
               Premium
             </a>
           </div>
-          {/* {!user && ( */}
+          {!user && (
             <Link
               href="/auth"
               // onClick={() =>
@@ -42,11 +43,24 @@ const Topbar: React.FC<TopbarProps> = () => {
                 Sign In
               </button>
             </Link>
-          {/* )} */}
+          )}
           {/* {user && problemPage && <Timer />} */}
-          {/* {user && ( */}
-            
-          {/* )} */}
+          {user && (
+            <div className="cursor-pointer group relative">
+              <img
+                src="/avatar.png"
+                alt="user profile"
+                className="h-8 w-8 rounded-full"
+              />
+              <div
+                className="absolute top-10 left-2/4 -translate-x-2/4  mx-auto bg-dark-layer-1 text-brand-orange p-2 rounded shadow-lg 
+								z-40 group-hover:scale-100 scale-0 
+								transition-all duration-300 ease-in-out"
+              >
+                <p className="text-sm">{user.email}</p>
+              </div>
+            </div>
+          )}
           {/* {user && <Logout />} */}
         </div>
       </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Topbar from "@/components/Topbar/Topbar";
-import ProblemsTable from "@/components/ProblemsTable";
+import ProblemsTable from "@/components/ProblemsTable/ProblemsTable";
 
 const inter = Inter({ subsets: ["latin"] });
 
