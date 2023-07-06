b58273b1d530a60fb6f3c79903ab6afa841dc330
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2023-07-06 22:03:11 +0300

    creating prob des

--- src/components/Workspace/Workspace.tsx
+++ src/components/Workspace/Workspace.tsx
@@ -4,25 +4,25 @@ import ProblemDescription from "./ProblemDescription/ProblemDescription";
 
 type WorkspaceProps = {};
 
-const Workspace: React.FC<WorkspaceProps> = (
+const Workspace: React.FC<WorkspaceProps> = () =>
   // { problem }
-  ) => {
-  // const { width, height } = useWindowSize();
-  // const [success, setSuccess] = useState(false);
-  // const [solved, setSolved] = useState(false);
+  {
+    // const { width, height } = useWindowSize();
+    // const [success, setSuccess] = useState(false);
+    // const [solved, setSolved] = useState(false);
 
-  return (
-    <Split className="split" minSize={0}>
-      <ProblemDescription 
-        // problem={problem} 
-        // _solved={solved} 
-      />
-      <div>The code editor will be here</div>
-      {/* <div className='bg-dark-fill-2'>
+    return (
+      <Split className="split" minSize={0}>
+        <ProblemDescription
+        // problem={problem}
+        // _solved={solved}
+        />
+        <div>The code editor will be here</div>
+        {/* <div className='bg-dark-fill-2'>
 				<Playground problem={problem} setSuccess={setSuccess} setSolved={setSolved} />
 				{success && <Confetti gravity={0.3} tweenDuration={4000} width={width - 1} height={height - 1} />}
 			</div>  */}
-    </Split>
-  );
-};
+      </Split>
+    );
+  };
 export default Workspace;
