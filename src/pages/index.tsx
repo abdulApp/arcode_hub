4bbee0ccbecfaba79cff824f16f884c617fd0469
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2023-07-07 21:05:32 +0300

    adding problems to db

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,15 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import Topbar from "@/components/Topbar/Topbar";
 import ProblemsTable from "@/components/ProblemsTable/ProblemsTable";
+import { useState } from "react";
+import { firestore } from "@/firebase/firebase";
+import { doc, setDoc } from "firebase/firestore";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  
+
   return (
     <>
       <main className="bg-dark-layer-2 min-h-screen">
@@ -26,42 +31,32 @@ export default function Home() {
           )} */}
           <table className="text-sm text-left text-gray-500 dark:text-gray-400 sm:w-7/12 w-full max-w-[1200px] mx-auto">
             {/* {!loadingProblems && ( */}
-              <thead className="text-xs text-gray-700 uppercase dark:text-gray-400 border-b ">
-                <tr>
-                  <th scope="col" className="px-1 py-3 w-0 font-medium">
-                    Status
-                  </th>
-                  <th scope="col" className="px-6 py-3 w-0 font-medium">
-                    Title
-                  </th>
-                  <th scope="col" className="px-6 py-3 w-0 font-medium">
-                    Difficulty
-                  </th>
+            <thead className="text-xs text-gray-700 uppercase dark:text-gray-400 border-b ">
+              <tr>
+                <th scope="col" className="px-1 py-3 w-0 font-medium">
+                  Status
+                </th>
+                <th scope="col" className="px-6 py-3 w-0 font-medium">
+                  Title
+                </th>
+                <th scope="col" className="px-6 py-3 w-0 font-medium">
+                  Difficulty
+                </th>
 
-                  <th scope="col" className="px-6 py-3 w-0 font-medium">
-                    Category
-                  </th>
-                  <th scope="col" className="px-6 py-3 w-0 font-medium">
-                    Solution
-                  </th>
-                </tr>
-              </thead>
+                <th scope="col" className="px-6 py-3 w-0 font-medium">
+                  Category
+                </th>
+                <th scope="col" className="px-6 py-3 w-0 font-medium">
+                  Solution
+                </th>
+              </tr>
+            </thead>
             {/* )} */}
-            <ProblemsTable 
-              // setLoadingProblems={setLoadingProblems} 
+            <ProblemsTable
+            // setLoadingProblems={setLoadingProblems}
             />
           </table>
         </div>
-        <form action="">
-          <input type="text" placeholder="problem id" name="id" />
-          <input type="text" placeholder="title" name="title" />
-          <input type="text" placeholder="difficulty" name="difficulty" />
-          <input type="text" placeholder="category" name="category" />
-          <input type="text" placeholder="order" name="order" />
-          <input type="text" placeholder="link?" name="link" />
-          <input type="text" placeholder="videoID?" name="videoID" />
-          <button>save to db</button>
-        </form>
       </main>
     </>
   );
