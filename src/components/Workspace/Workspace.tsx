a7da281e2f3947eb9e5705d5e4f781a9e64e6c63
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2023-07-08 14:44:30 +0300

    Confetti Celebration

--- src/components/Workspace/Workspace.tsx
+++ src/components/Workspace/Workspace.tsx
@@ -1,19 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Split from "react-split";
 import ProblemDescription from "./ProblemDescription/ProblemDescription";
 import Playground from "./Playground/Playground";
 import { Problem } from "@/utils/types/problem";
+import useWindowSize from "@/hooks/useWindowSize";
+import Confetti from "react-confetti";
 
 type WorkspaceProps = {
   problem: Problem;
 };
 
-const Workspace: React.FC<WorkspaceProps> = ({ problem }) =>
-  // { problem }
-  {
-    // const { width, height } = useWindowSize();
-    // const [success, setSuccess] = useState(false);
-    // const [solved, setSolved] = useState(false);
+const Workspace: React.FC<WorkspaceProps> = ({ problem }) => {
+    const { width, height } = useWindowSize();
+    const [success, setSuccess] = useState(false);
+    const [solved, setSolved] = useState(false);
 
     return (
       <Split className="split" minSize={0}>
@@ -23,9 +23,9 @@ const Workspace: React.FC<WorkspaceProps> = ({ problem }) =>
         // setSuccess={setSuccess}
         // setSolved={setSolved}
         />
-        {/* <div className='bg-dark-fill-2'>
+        <div className='bg-dark-fill-2'>
 				{success && <Confetti gravity={0.3} tweenDuration={4000} width={width - 1} height={height - 1} />}
-			</div>  */}
+			</div> 
       </Split>
     );
   };
