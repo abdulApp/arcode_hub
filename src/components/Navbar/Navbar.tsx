de2022c53e24ac0ad46cba9fcaa1bcc1f6a43b12
abdulrahman alamoudi <abdulrahman_alamoudi@hotmail.com>
2023-08-01 20:18:34 +0300

    update logo

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,27 +6,35 @@ import { useSetRecoilState } from "recoil";
 type NavbarProps = {};
 
 const Navbar: React.FC<NavbarProps> = () => {
-	const setAuthModalState = useSetRecoilState(authModalState);
-	const handleClick = () => {
-		setAuthModalState((prev) => ({ ...prev, isOpen: true }));
-	};
-	return (
-		<div className='flex items-center justify-between sm:px-12 px-2 md:px-24'>
-			<Link href='/' className='flex items-center justify-center h-20'>
-				<Image src='/logo.png' alt='LeetClone' height={200} width={200} />
-			</Link>
-			<div className='flex items-center'>
-				<button
-					className='bg-brand-orange text-white px-2 py-1 sm:px-4 rounded-md text-sm font-medium
+  const setAuthModalState = useSetRecoilState(authModalState);
+  const handleClick = () => {
+    setAuthModalState((prev) => ({ ...prev, isOpen: true }));
+  };
+  return (
+    <div className="flex items-center justify-between sm:px-12 px-2 md:px-24">
+      <Link href="/" className="flex items-center justify-center h-20">
+        {/* <Image src='/arcode.png' alt='LeetClone' height={200} width={200} />hub */}
+        <p className="text-white">
+          <span className="text-[#ff842a] text-xl">{"<"}</span>
+          <span className="border border-[#12c1ee] text-base rounded-md px-[2.5px] bg-[#12c1ee] text-black">
+            ar
+          </span>
+          CODE_<span className="text-[#12c1ee]">HUB</span>
+          <span className="text-[#ff842a] text-xl">{"/>"}</span>
+        </p>
+      </Link>
+      <div className="flex items-center">
+        <button
+          className="bg-brand-orange text-white px-2 py-1 sm:px-4 rounded-md text-sm font-medium
                 hover:text-brand-orange hover:bg-white hover:border-2 hover:border-brand-orange border-2 border-transparent
                 transition duration-300 ease-in-out
-                '
-					onClick={handleClick}
-				>
-					Sign In
-				</button>
-			</div>
-		</div>
-	);
+                "
+          onClick={handleClick}
+        >
+          Sign In
+        </button>
+      </div>
+    </div>
+  );
 };
 export default Navbar;
